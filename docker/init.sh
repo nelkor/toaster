11a8e31655b5d1ad570b799292f3c2bc4ab6992f
tyutyunnik.a <tk.nelkor@gmail.com>
2022-04-09 20:26:01 +0300

    maria

--- docker/init.sh
+++ docker/init.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
+rm -rf /hleb
 composer create-project phphleb/hleb /hleb
 cp /hleb/default.start.hleb.php /hleb/start.hleb.php
 chown -R www-data:www-data /hleb
+
+# Эта часть скопирована из /usr/local/bin/docker-php-entrypoint
+
+set -e
+
+if [ "${1#-}" != "$1" ]; then
+        set -- php-fpm "$@"
+fi
+
+exec "$@"
