687d10ddc82ff7cec8f398123728b19c2a8511c2
tyutyunnik.a <tk.nelkor@gmail.com>
2022-04-12 19:08:35 +0300

    php-cs-fixer

--- docker/init.php
+++ docker/init.php
@@ -3,5 +3,6 @@
 if (count(scandir('/hleb')) == 2) {
     exec('composer create-project phphleb/hleb /hleb');
     copy('/hleb/default.start.hleb.php', '/hleb/start.hleb.php');
+    exec('/root/.composer/vendor/bin/php-cs-fixer fix /hleb');
     exec('chown -R www-data:www-data /hleb');
 }
