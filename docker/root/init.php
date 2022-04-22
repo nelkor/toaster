4c9ef4c8c4351ad92fcf1af20e95541504a24d71
Foma Tuturov <fomiash@yandex.ru>
2022-04-23 00:07:05 +0300

    fix db port

--- docker/root/init.php
+++ docker/root/init.php
@@ -3,7 +3,7 @@
 if (count(scandir('/hleb')) == 2) {
     exec('composer create-project phphleb/hleb /hleb');
     copy('/hleb/default.start.hleb.php', '/hleb/start.hleb.php');
-    setDbEnvParams('DATABASE_EXTERNAL_PORT');
+    setDbEnvParams('MYSQL_DATABASE_PORT');
     setDbEnvParams('MYSQL_DATABASE');
     setDbEnvParams('MYSQL_USER');
     setDbEnvParams('MYSQL_PASSWORD');
