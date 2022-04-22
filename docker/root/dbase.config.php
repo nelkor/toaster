9acc9f7bc50b7b594f1ca2cf2da84030e620a84c
Foma Tuturov <fomiash@yandex.ru>
2022-04-22 22:27:16 +0300

    add env

--- docker/root/dbase.config.php
+++ docker/root/dbase.config.php
@@ -5,10 +5,10 @@ const HLEB_TYPE_DB = 'mariadb';
 const HLEB_PARAMETERS_FOR_DB = [
     'mariadb' => [
         'mysql:host=db',
-        'port=3306',
-        'dbname=hleb',
+        'port=DATABASE_EXTERNAL_PORT',
+        'dbname=MYSQL_DATABASE',
         'charset=utf8',
-        'user' => 'hleb',
-        'pass' => 'toaster',
+        'user' => 'MYSQL_USER',
+        'pass' => 'MYSQL_PASSWORD',
     ],
 ];
--- docker/root/init.php
+++ docker/root/init.php
@@ -4,10 +4,18 @@ if (count(scandir('/hleb')) == 2) {
     exec('composer create-project phphleb/hleb /hleb');
     copy('/hleb/default.start.hleb.php', '/hleb/start.hleb.php');
     rename('/root/dbase.config.php', '/hleb/database/dbase.config.php');
+    setDbEnvParams('DATABASE_EXTERNAL_PORT');
+    setDbEnvParams('MYSQL_DATABASE');
+    setDbEnvParams('MYSQL_USER');
+    setDbEnvParams('MYSQL_PASSWORD');
     rename('/root/.php-cs-fixer.php', '/hleb/.php-cs-fixer.php');
     exec('/root/.composer/vendor/bin/php-cs-fixer fix /hleb');
     exec('sh /root/www-data.sh');
 } else {
     unlink('/root/dbase.config.php');
     unlink('/root/.php-cs-fixer.php');
+}
+
+function setDbEnvParams($envName) {
+    file_put_contents('/root/dbase.config.php', str_replace($envName, getenv($envName), file('/root/dbase.config.php')));
 }
